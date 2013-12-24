7a9829ec6e30a8a403a81cb5f1c7232438cf824a
James Corbould <james.bron@hotmail.com>
2013-12-24 19:54:15 +1300

    Initial commit

---
+++ Ajax.WCF.RandomPresent/RandomPresentService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Ajax.WCF.RandomPresent
+{
+    [ServiceBehavior(Namespace = "http://schemas.ajax/randompresentservice")]
+    public class RandomPresentService : IRandomPresentService
+    {
+        public RandomPresentService()
+        {
+        }
+
+        public string ObtainPresent()
+        {
+            string[] presents = { "Moro bar", "Football", "Cabbage", "Drill press" };
+
+            Random r = new Random();
+            return presents[r.Next(presents.Length)];
+        }
+    }
+}
