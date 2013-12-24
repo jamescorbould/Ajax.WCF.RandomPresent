cc1b4f13fc5ab711cc72b75ff8a91c23f8cdab0b
James Corbould <james.bron@hotmail.com>
2013-12-24 21:39:20 +1300

    Added RandomPresentService IIS virtual dir

---
+++ RandomPresentService/App_Code/IRandomPresentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajax.WCF.RandomPresent
+{
+    [ServiceContract(Namespace = "http://schemas.ajax/randompresentservice")]
+    public interface IRandomPresentService
+    {
+        [OperationContract]
+        string ObtainPresent();
+    }
+}
